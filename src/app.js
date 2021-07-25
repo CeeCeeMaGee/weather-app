48ee9b19fb9f9925e6861940e193be568edb7ba0
CeeCeeMaGee <87149145+CeeCeeMaGee@users.noreply.github.com>
2021-07-25 18:19:29 -0400

    Added relevant icon

--- src/app.js
+++ src/app.js
@@ -20,15 +20,23 @@ let temperatureElement = document.querySelector("#temperature");
  let humidityElement= document.querySelector("#humidity");
  let windElement = document.querySelector("#wind");
  let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
+
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
-humidityElement.innerHTML = Math.round(response.data.main.humidity);
+humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
-dateElement.innerHTML = formatDate(response.data.dt * 1000);
+dateElement.innerHTML = formatDate(response.data.dt  *  1000);
+iconElement.setAttribute(
+    "src", 
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey ="ca6b966e1f2662438b430bcd9db95a1c";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=${apiKey}&units=metric`;
+let city= "Santorini";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
