0d0137ac9b116a782e0dd37c2251e602dcc8c567
CeeCeeMaGee <87149145+CeeCeeMaGee@users.noreply.github.com>
2021-07-26 13:51:20 -0400

    Added search engine

--- src/app.js
+++ src/app.js
@@ -35,8 +35,24 @@ iconElement.setAttribute(
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey ="ca6b966e1f2662438b430bcd9db95a1c";
-let city= "Sydney";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "ca6b966e1f2662438b430bcd9db95a1c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement  = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+
+
+
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit" , handleSubmit);
