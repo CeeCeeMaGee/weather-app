c7003a6ed54501c68b6c8f42da1920fc8e759432
CeeCeeMaGee <87149145+CeeCeeMaGee@users.noreply.github.com>
2021-07-26 12:36:09 -0400

    Update time

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function formatDate(timestamp) {
 let date = new Date(timestamp);
 let hours = date.getHours();
-if (hours ,10) {
+if (hours < 10) {
     hours = `0${hours}`;
 }
 let minutes = date.getMinutes();
@@ -36,7 +36,7 @@ iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey ="ca6b966e1f2662438b430bcd9db95a1c";
-let city= "Santorini";
+let city= "Delray Beach";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
